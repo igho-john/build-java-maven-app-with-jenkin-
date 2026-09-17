cb96e0d7b490b8603f1c49007bc7b14279990dc6
igho johh enakarhire <johnighoe@gmail.com>
2026-09-16 22:43:30 -0230

    Refactor build functions and add testApp function

--- Jenkinsfile-syntax/script.groovy
+++ Jenkinsfile-syntax/script.groovy
@@ -1,19 +1,20 @@
-def buildJar() {
+def buildApp() {
     echo "building the application..."
     sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t ighojohn/demo-app:jma-2.0.'
+        sh 'docker build -t ighojohn/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push ighojohn/demo-app:tagname'
+        sh 'docker push ighojohn/demo-app:jma-2.0'
     }
-} 
+}
+
+def testApp() {
+    echo "running tests..."
+    // add real test commands here later if needed
+}
 
 def deployApp() {
     echo 'deploying the application...'
-} 
+}
 
 return this
